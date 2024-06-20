624e933fd799d0525bbd6e29bb720058a9e28100
Ankit Kumar <ankitkr8540@gmail.com>
2024-06-19 22:22:03 -0400

    leetcode-calendar done

---
+++ example-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg'
+import './App.css'
+import LeetCodeCalendar from 'leet-calendar'
+function App() {
+  return (
+    <div className="App">
+      <LeetCodeCalendar username="ankitkr8540" />
+    </div>
+  )
+}
+
+export default App
